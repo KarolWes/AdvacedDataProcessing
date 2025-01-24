d0122ee1ba28a9490f3ca3b19c17dfb9f0cb9ac3
KarolWes <karol.wesolowski01@gmail.com>
2025-01-24 17:13:01 +0100

    lab12 part 2

---
+++ lab12/src/main/java/Index.java
@@ -0,0 +1,40 @@
+import org.apache.lucene.analysis.StopwordAnalyzerBase;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Index {
+
+
+
+    private static Document buildDoc(String title, String isbn) {
+        Document doc = new Document();
+        doc.add(new TextField("title", title, Field.Store.YES));
+        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        return doc;
+    }
+
+    static void createIndex(StopwordAnalyzerBase analyzer) throws IOException {
+        Directory directory = FSDirectory.open(Paths.get(Main.INDEX_DIRECTORY));
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter w = new IndexWriter(directory, config);
+
+        w.addDocument(buildDoc("Lucyna w akcji", "9780062316097"));
+        w.addDocument(buildDoc("Akcje rosną i spadają", "9780385545955"));
+        w.addDocument(buildDoc("Bo ponieważ", "9781501168007"));
+        w.addDocument(buildDoc("Naturalnie urodzeni mordercy","9780316485616"));
+        w.addDocument(buildDoc("Druhna rodzi", "9780593301760"));
+        w.addDocument(buildDoc("Urodzić się na nowo", "9780679777489"));
+
+        w.close();
+
+    }
+}
--- lab12/src/main/java/Main.java
+++ lab12/src/main/java/Main.java
@@ -1,24 +1,11 @@
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.store.ByteBuffersDirectory;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.analysis.pl.PolishAnalyzer;
 
 import java.io.IOException;
 
 public class Main {
+
+    public static final String INDEX_DIRECTORY = "F:\\Java\\AdvacedDataProcessing\\lab12\\indecies";
 
     String[] queries = {
             "dummy",
@@ -35,9 +22,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
         PolishAnalyzer analyzer = new PolishAnalyzer();
-        Directory directory = new ByteBuffersDirectory();
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-        IndexWriter w = new IndexWriter(directory, config);
+        Index.createIndex(analyzer);
+
 
 //        w.addDocument(buildDoc("Lucene in Action", "9781473671911"));
 //        w.addDocument(buildDoc("Lucene for Dummies", "9780735219090"));
@@ -45,40 +31,13 @@ public class Main {
 //        w.addDocument(buildDoc("The Art of Computer Science", "9781250301695"));
 //        w.addDocument(buildDoc("Dummy and yummy title", "9780525656161"));
 
-        w.addDocument(buildDoc("Lucyna w akcji", "9780062316097"));
-        w.addDocument(buildDoc("Akcje rosną i spadają", "9780385545955"));
-        w.addDocument(buildDoc("Bo ponieważ", "9781501168007"));
-        w.addDocument(buildDoc("Naturalnie urodzeni mordercy","9780316485616"));
-        w.addDocument(buildDoc("Druhna rodzi", "9780593301760"));
-        w.addDocument(buildDoc("Urodzić się na nowo", "9780679777489"));
 
-        w.close();
 
         String querystr = "naturanle~0.5";
-        Query q = new QueryParser("title", analyzer).parse(querystr);
+        Search.search(querystr, analyzer);
 
-        int maxHits = 10;
-        IndexReader reader = DirectoryReader.open(directory);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        TopDocs docs = searcher.search(q, maxHits);
-        ScoreDoc[] hits = docs.scoreDocs;
-
-        System.out.println("Found " + hits.length + " matching docs.");
-        StoredFields storedFields = searcher.storedFields();
-        for(int i=0; i<hits.length; ++i) {
-            int docId = hits[i].doc;
-            Document d = storedFields.document(docId);
-            System.out.println((i + 1) + ". " + d.get("isbn")
-                    + "\t" + d.get("title"));
-        }
-        reader.close();
     }
 
-    private static Document buildDoc(String title, String isbn) {
-        Document doc = new Document();
-        doc.add(new TextField("title", title, Field.Store.YES));
-        doc.add(new StringField("isbn", isbn, Field.Store.YES));
-        return doc;
-    }
+
 
 }
